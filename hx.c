ba7ac4cc56207fb62c7f6f897162d5eea09c14ad
yalem123 <yalembrhankelayneh@gmail.com>
2022-05-17 11:36:23 -0700

    printf

---
+++ hx.c
@@ -0,0 +1,68 @@
+/**
+ * AUTHORS: yalem kelayneh, hamade jibril.
+ */
+#include "main.h"
+unsigned int convert_x(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_X(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+/**
+ * convert_x - Converts an unsigned int argument to hex using abcdef.
+ * @args: list of argument to be converted.
+ * @flags: Flag modifiers.
+ * @wid: A width modifier.
+ * @prec: A precision modifier.
+ * @len: A length modifier.
+ * @output: A buffer_t struct containing a char array.
+ * Return: The number of bytes stored to the buffer.
+ */
+unsigned int convert_x(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len)
+{
+	unsigned long int num;
+	unsigned int ret = 0;
+	char *lead = "0x";
+if (len == LONG)
+num = va_arg(args, unsigned long int);
+	else
+		num = va_arg(args, unsigned int);
+	if (len == SHORT)
+		num = (unsigned short)num;
+	if (HASH_FLAG == 1 && num != 0)
+		ret += _memcpy(output, lead, 2);
+	if (!(num == 0 && prec == 0))
+		ret += convert_ubase(output, num, "0123456789abcdef",
+				flags, wid, prec);
+	ret += print_neg_width(output, ret, flags, wid);
+	return (ret);
+}
+/**
+ * convert_X - Converts an unsigned int argument to hex using ABCDEF.
+ * @args: list of argument to be converted
+ * @flags: Flag modifiers.
+ * @wid: A width modifier.
+ * @prec: A precision modifier.
+ * @len: A length modifier.
+ * @output: A buffer_t struct containing a char array.
+ * Return: The number of bytes stored to the buffer.
+ */
+unsigned int convert_X(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len)
+{
+	unsigned long int num;
+	unsigned int ret = 0;
+	char *lead = "0X";
+if (len == LONG)
+num = va_arg(args, unsigned long);
+	else
+		num = va_arg(args, unsigned int);
+	if (len == SHORT)
+		num = (unsigned short)num;
+	if (HASH_FLAG == 1 && num != 0)
+		ret += _memcpy(output, lead, 2);
+	if (!(num == 0 && prec == 0))
+		ret += convert_ubase(output, num, "0123456789ABCDEF",
+				flags, wid, prec);
+	ret += print_neg_width(output, ret, flags, wid);
+	return (ret);
+}
